cc6f086ac5ffe25a40af5faa5235dccbf0b102eb
Maksym Iliev <orioniv30@gmail.com>
2024-11-13 19:17:38 +0300

    10.3

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const user = {
-    name: "Maksym",
-    surname: "Iliev",
-    age: 18,
-    country: "Turkey",
-    city: "Antalya",
+const contactBook = {
+    contacts:[{
+        name: 'Maksim Iliev',
+        phone: +'905348823617',
+        mail: 'maksim.iliev@icloud.com'
+    },
+        {
+            name: 'Daria Dobrov',
+            phone: '+380984165822',
+            mail: 'dobrov.daria@gmail.com'
+        }],
+    findContactByName: function (name){
 
-    getUserInfo: function () {
-        return `Name: ${this.name}\nAge: ${this.age}\nCountry: ${this.country}\nCity: ${this.city}`;
-    }
+        return this.contacts.filter(contact => contact.name.toLowerCase() === name.toLowerCase());
+    },
+      addContact: function (name,phone,mail){
+      const newContact= {name, phone, mail};
+      this.contacts.push(newContact);
+      console.log("New contact", newContact);
+      }
 }
-console.log(user.getUserInfo());
+
+console.log(contactBook.findContactByName("Maksim Iliev"));
+contactBook.addContact("Valera Pisakov","+35950025545421", "ololo@mail.com");
+console.log(contactBook.contacts);
