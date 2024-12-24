76fb9d6efef59210aee2e4ed6cc46dd1d881a523
ffrickk <103633893+ori0niv@users.noreply.github.com>
2024-12-24 18:19:52 +0300

    20.1

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-class Coach {
-    constructor(name, specialization, rating) {
-        this.name = name;
-        this.specialization = specialization;
-        this.rating = rating;
-    }
-
-    displayInfo() {
-        console.log(`Coach: ${this.name}, Specialization: ${this.specialization}, Rating: ${this.rating}`);
-    }
-}
-
-const coach1 = new Coach('John Art', 'CrossFit', 4.7);
-const coach2 = new Coach('Alice Born', 'dance', 4.9);
-
-coach1.displayInfo();
-coach2.displayInfo();
+$(document).ready(function () {
+  $('.todo-item').on('click', function () {
+    const taskText = $(this).data('task');
+    $('#taskModalBody').text(taskText); 
+    $('#taskModal').modal('show'); 
+  });
+});
