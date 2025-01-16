a00de69029845cbcef3a73cfde3bc11998957f78
ffrickkk <maksim.iliev@icloud.com>
2025-01-16 19:21:10 +0200

    first commit

---
+++ horror-movies/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HorrorMovies from './HorrorMovies';
+
+function App() {
+  return (
+      <div className="App">
+        <HorrorMovies />
+      </div>
+  );
+}
+
+export default App;
