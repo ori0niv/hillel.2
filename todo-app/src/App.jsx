419f93177770e6f96679deba71c1c8501c942791
ffrickkk <maksim.iliev@icloud.com>
2025-02-04 18:22:39 +0200

    finish

---
+++ todo-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TodoApp from "./TodoApp";
+
+function App() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-200">
+            <TodoApp />
+        </div>
+    );
+}
+
+export default App;
